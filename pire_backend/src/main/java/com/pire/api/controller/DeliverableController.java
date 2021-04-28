0eefd887f42d9e68ce47dd83979230f1c330bb54
kaanatesel <kaanatesel99@gmail.com>
2021-04-28 16:17:52 +0300

    Cross-Origin Request Blockek solution and run conf is added

--- pire_backend/src/main/java/com/pire/api/controller/DeliverableController.java
+++ pire_backend/src/main/java/com/pire/api/controller/DeliverableController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.pire.api.servise.DeliverableService;
 
 @RestController
 @RequestMapping(path = "api/deliverable")
+@CrossOrigin("*")
 public class DeliverableController {
 	@Autowired
 	DeliverableService service;
