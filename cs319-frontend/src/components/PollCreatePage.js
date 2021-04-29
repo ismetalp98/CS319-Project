1af3f3331c60667356289455308b359a39f9ecbf
Kerem Alemdar <kerem.alemdar.54.60@hotmail.com>
2021-04-29 22:46:53 +0300

    PollCreationPage added

---
+++ cs319-frontend/src/components/PollCreatePage.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import "../csss/PollCreatePage.css";
+import AddIcon from "@material-ui/icons/Add";
+import Icon from "@material-ui/core/Icon";
+import OptionList from "./PollOption";
+
+class PollCreatePage extends Component {
+  state = {
+    OptionDetails: [
+      {
+        index: Math.random(),
+        option: ""
+      }
+    ]
+  };
+  addOption = e => {
+    this.setState(prevState => ({
+      OptionDetails: [
+        ...prevState.OptionDetails,
+        {
+          index: Math.random,
+          oprtion: ""
+        }
+      ]
+    }));
+  };
+
+  deleteOption(record) {
+    this.setState({
+      OptionDetails: this.state.OptionDetails.filter(r => r !== record)
+    });
+  }
+  render() {
+    let { OptionDetails } = this.state;
+    return (
+      <form className="PollForm">
+        <div className="PollCreation">
+          <div className="PirePollHeader">
+            <h2>Pire Poll</h2>
+            <Button id="addOptionBtn" onClick={() => this.addOption()}>
+              <AddIcon />
+            </Button>
+          </div>
+          <div className="PollGeneral">
+            <label htmlFor="question" />
+            <div className="search_form_div">
+              <div className="input">
+                <input
+                  id="option"
+                  placeholder="Type your question here"
+                  onFocus={e => (e.target.placeholder = "")}
+                  onBlur={e =>
+                    (e.target.placeholder = "Type your question here")
+                  }
+                  autoComplete="off"
+                  type="text"
+                />
+              </div>
+            </div>
+            <hr />
+            <div>
+              <div className="row">
+                <div>
+                  <OptionList
+                    add={this.addOption}
+                    delete={this.deleteOption.bind(this)}
+                    OptionDetails={OptionDetails}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <Button id="submitBtn" color="#841584">
+            Submit
+          </Button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default PollCreatePage;
---
+++ cs319-frontend/src/components/PollOption.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import "../csss/PollCreatePage.css";
+import AddIcon from "@material-ui/icons/Add";
+import MinusIcon from "@material-ui/icons/Remove";
+import Icon from "@material-ui/core/Icon";
+
+function addPro() {
+    this.props.add();
+  }
+const OptionList = props => {
+  return props.OptionDetails.map((val, idx) => {
+    let option = `option-${idx}`;
+    return (
+      <div className="search_form_div">
+        <div className="input">
+          <input
+            id="option"
+            placeholder="Enter poll option"
+            onFocus={e => (e.target.placeholder = "")}
+            onBlur={e => (e.target.placeholder = "Enter poll option")}
+            autoComplete="off"
+            type="text"
+          />
+        </div>
+        {idx === 0 ? (
+          <div />
+        ) : (
+          <Button id="deleteOptionBtn" onClick={() => props.delete(val)}>
+            <MinusIcon />
+          </Button>
+        )}
+      </div>
+    );
+  });
+};
+
+export default OptionList;
