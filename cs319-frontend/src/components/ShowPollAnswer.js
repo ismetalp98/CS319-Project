6ab6a658f184599da5a9a0d086ab6acbc1f3c874
Eylül Çağlar <caglar.eylul99@gmail.com>
2021-05-02 23:32:04 +0300

    Poll answer

---
+++ cs319-frontend/src/components/ShowPollAnswer.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "../csss/PollAnswer.css";
+import { Redirect } from "react-router-dom";
+import QuestionItem from "../items/QuestionItem";
+
+
+class ShowPollAnswer extends Component {
+  state = {};
+
+  getQuestions = e => {
+    var pollIndex = localStorage.getItem("currentPollIndex");
+    var pollQuestions;
+    var xhrpolls = new XMLHttpRequest();
+    xhrpolls.open("GET", "https://d7c59928777f.ngrok.io/api/poll/" + pollIndex);
+    xhrpolls.send();
+    xhrpolls.addEventListener("load", () => {
+      // update the state of the component with the result here
+      var parsed = JSON.parse(xhrpolls.response);
+      pollQuestions = parsed.questions;
+      console.log(pollQuestions);
+
+      const polls = pollQuestions.map(questionobj => {
+        return <QuestionAnswerItem
+          key={questionobj.id}
+          index={questionobj.id}
+          question={questionobj.question}
+          answer={questionobj.answers}>
+        </QuestionAnswerItem>
+      })
+      this.setState({ polls: polls });
+    });
+  };
+  componentDidMount() {
+    this.getQuestions();
+  }
+  render() {
+    if (this.state.questionAnswerEnded) {
+      return <Redirect to={'/homePage'} />;
+    }
+    return (
+      <form className="PollAnswer">
+        <div className="poll_answers_div">
+          <h1>{localStorage.getItem("currentPollName")}</h1>
+          <hr />
+          {this.state.polls}
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ShowPollAnswer;
---
+++ cs319-frontend/src/items/QuestionAnswerItem.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import "../csss/questionItem.css";
+import Button from "@material-ui/core/Button";
+
+class QuestionAnswerItem extends Component {
+  state = {};
+  componentDidMount() {
+    this.setState({ "name": this.props.question });
+    this.setState({ "index": this.props.index });
+  }
+
+  handleAnswer = e => {
+    let answer = document.getElementById(this.props.index).value;
+
+    var data = {
+      "pollQuestionId": this.props.index,
+      "studentEmail": localStorage.getItem("selectedMember"),
+      "answer": answer
+    };
+    var json = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+      if (xhr.status === 200) {
+      }
+    });
+    xhr.open("POST", "http://d7c59928777f.ngrok.io/api/poll/createPollAnswer");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(json);
+  };
+  getAnswer = e => {
+    var data = {
+      "pollQuestionId": this.props.index,
+      "studentEmail": localStorage.getItem("selectedMember"),
+      "answer": answer
+    };
+    var json = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+      if (xhr.status === 200) {
+      }
+    });
+    xhr.open("GET", "http://d7c59928777f.ngrok.io/api/poll/" + this.props.index);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(json);
+  };
+  
+  render() {
+    return (
+      <div className="question_answer_div">
+        <div className="question_item_name">
+          <h3 id="question_name" >{this.state.name}</h3>
+        </div>
+        <div className="question_form_div">
+          <div className="input">
+            <input
+              id={this.props.index}
+              placeholder="Answer"
+              autoComplete="off"
+              type="text"
+            />
+          </div>
+        </div>
+        <Button id="asnwer_submit_btn" onClick={this.handleAnswer} variant="contained" color="primary">
+          Submit
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default QuestionAnswerItem;
+
